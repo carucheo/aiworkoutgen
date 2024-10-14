fce34ab07084bb3c4c84421670148cfaa997d542
spaidafora <spaidafora@gmail.com>
2024-10-14 12:00:01 -0700

    Added and linked login/signup templates

---
+++ aiworkoutgen/core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),  # Root URL for the 'home' view
+    path('login', views.user_login, name='login'),    
+    path('signup', views.user_signup, name='signup'),
+
+
+]
+
