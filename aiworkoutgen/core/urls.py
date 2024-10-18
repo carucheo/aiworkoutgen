4c5080f52e64af17dc7659a4a3de29850bd0d68f
spaidafora <spaidafora@gmail.com>
2024-10-17 21:22:08 -0700

    Added API integration, getting json format only

--- aiworkoutgen/core/urls.py
+++ aiworkoutgen/core/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Root URL for the 'home' view
+    path('', views.home, name='home'), #  Root URL for the 'home' view
     path('login', views.user_login, name='login'),    
     path('signup', views.user_signup, name='signup'),
-
+    path('workout', views.user_workout, name='workout'),
 
 ]
-
